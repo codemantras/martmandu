085f1746b8f1c00b193d5f6685abfe518ba64aae
Divya Shrestha <work@divyashrestha.com.np>
2025-07-19 08:38:34 +0545

    added migration and model (#1)

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+    protected $attributes= ['order_id', 'product_id', 'quantity', 'unite_price', 'total_amount'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
